4d119859d90c4b746a2b5ad79e1acc2316880467
space-runner <dalex2525@hotmail.com>
2019-03-30 23:06:45 -0400

    version 1

---
+++ Helpers/HTMLHelpers/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using HTMLHelpers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTMLHelpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Personas()
+        {
+            return View();
+        }
+
+        public ActionResult DetallePersonas(datosPersona persona)
+        {
+            return View(persona);
+        }
+    }
+}
---
+++ Helpers/HTMLHelpers/Models/datosPersona.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HTMLHelpers.Models
+{
+    public class datosPersona
+    {
+        [Required]
+        public string Cedula { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        public string Apellido { get; set; }
+
+        [Range(15,99)]
+        public int Edad { get; set; }
+
+        public string Telefono { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Correo  { get; set; }
+
+        [Display(Name ="Genero")]
+        public Genero genero { get; set; }
+
+        [Display(Name ="Estado Civil")]
+        public string EstadoCivil { get; set; }
+
+        public string[] Hobbys { get; set; }
+
+    }
+
+    public enum Genero
+    {
+        Masculino,
+        Femenino
+    }
+
+}
